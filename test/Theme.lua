a9fd42cdc15d48dda950a97dc012c621735495f2
onehalla <realonehalla@gmail.com>
2022-12-03 16:30:09 +0700

    asasdsad

---
+++ test/Theme.lua
@@ -0,0 +1,74 @@
+local ThemeList = {
+	["Default"] = {
+		Banner = "11220387429",
+		MainColor = Color3.fromRGB(85, 255, 127),
+		AccentColor = Color3.fromRGB(85, 255, 127),
+		FontColor = Color3.fromRGB(255, 255, 255),
+		Gradient = true,
+		BackgroundColor = Color3.fromRGB(40, 40, 40),
+		LeftBarColor = Color3.fromRGB(30, 30, 30),
+		TopbarColor = Color3.fromRGB(20, 20, 20),
+		ContainerBackground = Color3.fromRGB(100, 100, 100),
+		TabContainerColor = Color3.fromRGB(30, 30, 30),
+		SectionFrameColor = Color3.fromRGB(85, 255, 127),
+
+		SliderBackgroundColor = Color3.fromRGB(30, 30, 30),
+		DropdownBackgroundColor = Color3.fromRGB(60, 60, 60),
+		-- Toggle
+		ToggleBackgroundColor_Off = Color3.fromRGB(255, 83, 120),
+		ToggleBackgroundColor_On = Color3.fromRGB(85, 255, 127),
+		ToggleDotColor_Off = Color3.fromRGB(255, 255, 255),
+		ToggleDotColor_On = Color3.fromRGB(30, 30, 30),
+
+		-- Button
+		ButtonTextColor = Color3.fromRGB(0, 0, 0),
+	},
+	["Hutao"] = {
+		Banner = "11499908803",
+		MainColor = Color3.fromRGB(255, 182, 80),
+		AccentColor = Color3.fromRGB(255, 182, 80),
+		FontColor = Color3.fromRGB(255, 255, 255),
+		Gradient = true,
+		BackgroundColor = Color3.fromRGB(40, 40, 40),
+		LeftBarColor = Color3.fromRGB(30, 30, 30),
+		TopbarColor = Color3.fromRGB(20, 20, 20),
+		ContainerBackground = Color3.fromRGB(100, 100, 100),
+		TabContainerColor = Color3.fromRGB(30, 30, 30),
+		SectionFrameColor = Color3.fromRGB(255, 182, 80),
+
+		SliderBackgroundColor = Color3.fromRGB(30, 30, 30),
+		DropdownBackgroundColor = Color3.fromRGB(60, 60, 60),
+		-- Toggle
+		ToggleBackgroundColor_Off = Color3.fromRGB(255, 83, 120),
+		ToggleBackgroundColor_On = Color3.fromRGB(255, 182, 80),
+		ToggleDotColor_Off = Color3.fromRGB(255, 255, 255),
+		ToggleDotColor_On = Color3.fromRGB(30, 30, 30),
+
+		-- Button
+		ButtonTextColor = Color3.fromRGB(0, 0, 0),
+	},
+	["Rem"] = {
+		Banner = "11726404109",
+		MainColor = Color3.fromRGB(86, 180, 252),
+		AccentColor = Color3.fromRGB(115, 189, 245),
+		FontColor = Color3.fromRGB(255, 255, 255),
+		Gradient = true,
+		BackgroundColor = Color3.fromRGB(40, 40, 40),
+		LeftBarColor = Color3.fromRGB(30, 30, 30),
+		TopbarColor = Color3.fromRGB(20, 20, 20),
+		ContainerBackground = Color3.fromRGB(100, 100, 100),
+		TabContainerColor = Color3.fromRGB(30, 30, 30),
+		SectionFrameColor = Color3.fromRGB(86, 180, 252),
+
+		SliderBackgroundColor = Color3.fromRGB(30, 30, 30),
+		DropdownBackgroundColor = Color3.fromRGB(60, 60, 60),
+		-- Toggle
+		ToggleBackgroundColor_Off = Color3.fromRGB(255, 83, 120),
+		ToggleBackgroundColor_On = Color3.fromRGB(86, 180, 252),
+		ToggleDotColor_Off = Color3.fromRGB(255, 255, 255),
+		ToggleDotColor_On = Color3.fromRGB(30, 30, 30),
+
+		-- Button
+		ButtonTextColor = Color3.fromRGB(0, 0, 0),
+	},
+}
