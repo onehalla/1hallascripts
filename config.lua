6019dafda1b8e947bf6683870cc1a507c79fa901
onehalla <realonehalla@gmail.com>
2022-12-03 21:54:36 +0700

    update config.lua and viperhub.lua

--- config.lua
+++ config.lua
@@ -6,34 +6,39 @@ return {
     Seed = 0,
     Steps = {
         {
-            Name = "Vmify",
-            Settings = {},
-        },
-        {
-            Name = "EncryptStrings",
-            Settings = {},
-        },
-        {
-            Name = "Vmify",
-            Settings = {},
-        },
-        {
-            Name = "ConstantArray",
+            Name = "EncryptStrings";
             Settings = {
-                Treshold = 1,
-                StringsOnly = true,
-                Shuffle = true,
-                Rotate = true,
-                LocalWrapperTreshold = 0,
-            },
+
+            };
+        },
+        
+        {
+            Name = "Vmify";
+            Settings = {
+                
+            };
         },
         {
-            Name = "NumbersToExpressions",
-            Settings = {},
+            Name = "ConstantArray";
+            Settings = {
+                Treshold    = 1;
+                StringsOnly = true;
+                Shuffle     = true;
+                Rotate      = true;
+                LocalWrapperTreshold = 0;
+            }
         },
         {
-            Name = "WrapInFunction",
-            Settings = {},
+            Name = "NumbersToExpressions";
+            Settings = {
+
+            }
         },
-    },
+        {
+            Name = "WrapInFunction";
+            Settings = {
+
+            }
+        },
+    }
 }
